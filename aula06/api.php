e1171f8ea3b0ade6715b5ba4321789e3f9723fbf
Felipejbm <felipejbmello.etec@gmail.com>
2026-04-23 09:23:52 -0300

    aula23/04

--- aula06/api.php
+++ aula06/api.php
@@ -1,29 +1,46 @@
 <?php
 
-    // header("content-type: application/json"); 
+header("Content-Type: application/json; charset=UTF-8");
 
-    // $usuarios = [
-    //     ["id" => 1, "nome" => "Fefe ", "email"=> "fefe@email.com"],
-    //     ["id" => 2, "nome" => "Dandan", "email"=> "dandan@email.com"]
-    // ];
-    
-    // echo json_encode($usuarios);
-    
-    header("content-type: application/json"); 
+$metodo = $_SERVER['REQUEST_METHOD'];
 
-    $metodo = $_SERVER['REQUEST_METHOD'];
+$arquivo = 'usuarios.json';
 
-    // echo "metodo de requisição " .$metodo;
- 
-    switch ($metodo) {
-        case 'GET':
-            echo "here GET method actions";
-            break;
-        case 'POST';
-            echo "here POST method actions"; 
-            break;
-        default:
-            echo 'method not found!';
-            break;
-    }
+if (!file_exists($arquivo)) {
+    file_put_contents($arquivo, json_encode([], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+}
+
+$usuarios = json_decode(file_get_contents($arquivo), true);
+
+switch ($metodo) {
+    case 'GET':
+        echo json_encode($usuarios, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+        break;
+    case 'POST':
+        $dados = json_decode(file_get_contents('php://input'), true);
+
+        if (!isset($dados["id"]) || !isset($dados["nome"]) || !isset($dados["email"])) {
+            http_response_code(400);
+            echo json_encode(["erro" => "Dados incompletos."], JSON_UNESCAPED_UNICODE);
+            exit;
+        }
+
+        $novoUsuario = [
+            "id" => $dados["id"],
+            "nome" => $dados["nome"],
+            "email" => $dados["email"]
+        ];
+
+        $usuarios[] = $novoUsuario;
+
+        file_put_contents($arquivo, json_encode($usuarios, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+        break;
+    default:
+        http_response_code(405);
+        echo json_encode(["erro" => "Método não permitido!"], JSON_UNESCAPED_UNICODE);
+        break;
+
+}
+
+
 ?>
